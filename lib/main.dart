dfa212805030a561efee7878357ded5fcbee6e53
susumOyaji <sumitomo0210@gmail.com>
2020-12-15 17:46:19 +0900

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,109 +13,169 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "Face Finder",
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: FaceFinder(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class FaceFinder extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<StatefulWidget> createState() => _FaceFinderState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _FaceFinderState extends State<FaceFinder> {
+  File _imageFile;
+  Size _imageSize;
+  List<Face> _faceResults;
+  final FaceDetector _faceDetector = FirebaseVision.instance.faceDetector();
+  final ImagePicker _picker = ImagePicker();
 
-  void _incrementCounter() {
+  void _getImageAndFindFace(ImageSource imageSource) async {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _imageFile = null;
+      _imageSize = null;
     });
+
+    final PickedFile pickedImage = await _picker.getImage(source: imageSource);
+    final File imageFile = File(pickedImage.path);
+
+    if (imageFile != null) {
+      _getImageSize(imageFile);
+      _findFace(imageFile);
+    }
+
+    setState(() {
+      _imageFile = imageFile;
+    });
+  }
+
+  void _getImageSize(File imageFile) {
+    final Image image = Image.file(imageFile);
+    image.image.resolve(ImageConfiguration()).addListener(
+      ImageStreamListener((ImageInfo info, bool _) {
+        setState(() {
+          _imageSize = Size(
+            info.image.width.toDouble(),
+            info.image.height.toDouble(),
+          );
+        });
+      }),
+    );
+  }
+
+  void _findFace(File imageFile) async {
+    setState(() {
+      _faceResults = null;
+    });
+
+    final FirebaseVisionImage visionImage =
+        FirebaseVisionImage.fromFile(imageFile);
+
+    List<Face> results = await _faceDetector.processImage(visionImage);
+    setState(() {
+      _faceResults = results;
+    });
+  }
+
+  Widget _makeImage() {
+    return Container(
+        constraints: BoxConstraints.expand(),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: Image.file(_imageFile).image,
+            fit: BoxFit.contain,
+          ),
+        ),
+        child: _imageSize == null || _faceResults == null
+            ? Center(
+                child: Text(
+                  "Detecting...",
+                  style: TextStyle(
+                    color: Colors.lightBlue,
+                    fontSize: 32.0,
+                  ),
+                ),
+              )
+            : CustomPaint(painter: FaceBorderDrawer(_imageSize, _faceResults)));
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+        appBar: AppBar(
+          title: Text("Face Finder"),
+        ),
+        body: _imageFile == null
+            ? Center(child: Text("No image selected."))
+            : _makeImage(),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            FloatingActionButton(
+              onPressed: () {
+                _getImageAndFindFace(ImageSource.gallery);
+              },
+              tooltip: "Select Image",
+              child: Icon(Icons.add_photo_alternate),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Padding(padding: EdgeInsets.all(10.0)),
+            FloatingActionButton(
+              onPressed: () {
+                _getImageAndFindFace(ImageSource.camera);
+              },
+              tooltip: "Take Photo",
+              child: Icon(Icons.add_a_photo),
             ),
           ],
+        ));
+  }
+
+  @override
+  void dispose() {
+    _faceDetector.close();
+    super.dispose();
+  }
+}
+
+class FaceBorderDrawer extends CustomPainter {
+  final Size originalImageSize;
+  final List<Face> faces;
+
+  FaceBorderDrawer(this.originalImageSize, this.faces);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final double scale = size.width / originalImageSize.width;
+    final double vSpace = (size.height -
+            size.width * originalImageSize.height / originalImageSize.width) /
+        2.0;
+
+    final Paint paint = Paint();
+    paint.style = PaintingStyle.stroke;
+    paint.strokeWidth = 2.0;
+    paint.color = Colors.red;
+
+    for (Face face in faces) {
+      canvas.drawRect(
+        Rect.fromLTRB(
+          face.boundingBox.left * scale,
+          face.boundingBox.top * scale + vSpace,
+          face.boundingBox.right * scale,
+          face.boundingBox.bottom * scale + vSpace,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        paint,
+      );
+    }
+  }
+
+  @override
+  bool shouldRepaint(FaceBorderDrawer oldDelegate) {
+    return oldDelegate.originalImageSize != originalImageSize ||
+        oldDelegate.faces != faces;
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ai_app/main.dart';
+import 'package:ai_app/section_3/1_simple_app/1-1_main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
